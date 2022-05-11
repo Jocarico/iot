49ce9a906242ac1c3a0281a7351cd62857d6071d
Jonatan Rico <jonatancalebrico@gmail.com>
2022-05-10 21:34:07 -0600

    First flask app

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+@author J.Rico
+@since 2022-05-10
+@descrip Ejercicio de Flask
+"""
+
+# Inclusion de modulos
+
+from flask import Flask
+
+app = Flask('__main__')
+
+@app.route('/', methods=['GET'])
+
+def go_home():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, host='0.0.0.0')
